958902c82d4411fa51e7ae630d44956bb52bd703
Brian Wallenrod <bwallenrod321@gmail.com>
2026-04-24 13:52:45 -0700

    updated methods

--- src/main/java/ProjectController.java
+++ src/main/java/ProjectController.java
@@ -1,13 +1,3 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.FileWriter;
-import java.io.IOException;
-import com.google.gson.reflect.TypeToken;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ProjectController {
 
     public Project createProject(int id, String title, String summary, String type) {
@@ -15,24 +5,7 @@ public class ProjectController {
     }
 
     public void saveProject(Project project) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        List<Project> projects = loadProjects();
-        projects.add(project);
-        try (FileWriter writer = new FileWriter("projects.json")) {
-            gson.toJson(projects, writer);
-        } catch (IOException e) {
-            System.out.println("Error saving project: " + e.getMessage());
-        }
-    }
-
-    public List<Project> loadProjects() {
-        Gson gson = new Gson();
-        try (FileReader reader = new FileReader("projects.json")) {
-            Type listType = new TypeToken<List<Project>>(){}.getType();
-            return gson.fromJson(reader, listType);
-        } catch (IOException e) {
-            return new ArrayList<>();
-        }
+        Blackboard.getInstance().addProject(project);
     }
 
 }
