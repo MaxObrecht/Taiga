15b18604910da5226204130b62f458fca02a7b6a
Tyler Page <tyler98815@gmail.com>
2026-05-04 13:13:51 -0700

    Added task edit panel to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         ViewsManager vm = ViewsManager.getInstance();
+        TaskUI taskUI = new TaskUI();
+
         vm.addPanel(new LoginUI(), "Login");
         vm.addPanel(new ProjectUI(), "Project");
         vm.addPanel(new UserStoryUI(), "UserStory");
         //vm.addPanel(new SprintUI(), "Sprint");
-        //vm.addPanel(new TaskUI(), "Task");
+        vm.addPanel(taskUI, "Task");
+        vm.addPanel(taskUI.editPanel(), "TaskEdit");
         vm.addPanel(new TestUI(), "Test");
 
         vm.start("Login");
