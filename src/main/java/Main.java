98a1ac15205e0a1127f54bf7211956ac27b2f62b
Brian Wallenrod <bwallenrod321@gmail.com>
2026-05-04 12:49:19 -0700

    added Login to vm created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         ViewsManager vm = ViewsManager.getInstance();
-
+        vm.addPanel(new LoginUI(), "Login");
         vm.addPanel(new ProjectUI(), "Project");
         vm.addPanel(new UserStoryUI(), "UserStory");
         //vm.addPanel(new SprintUI(), "Sprint");
         //vm.addPanel(new TaskUI(), "Task");
         vm.addPanel(new TestUI(), "Test");
 
-        vm.start("Project");
+        vm.start("Login");
     }
 }
